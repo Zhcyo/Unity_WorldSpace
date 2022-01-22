ace8cc34faa4cc3d8416b90601aa6703f698988d
Zhcyo <1251070979@qq.com>
2022-01-22 15:09:43 +0800

    update

---
+++ Space/Assets/Lobby/Scripts/Singleton/Singleton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T:MonoBehaviour
+{
+    private static T _instance;
+    public static T Intance {
+        get {
+            if (_instance == null)
+            {
+                GameObject obj = new GameObject();
+                obj.name = string.Format("_{0}", typeof(T).Name);
+                _instance = obj.AddComponent<T>();
+            }
+            return _instance;
+        }
+    
+    }
+    protected virtual void Awake()
+    {
+        _instance = this as T;
+    }
+}
--- Space_clone_0/Assets/Scripts/PlayerMove.cs
+++ Space_clone_0/Assets/Scripts/PlayerMove.cs
@@ -2,43 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
+using Cinemachine;
 
 public class PlayerMove :NetworkBehaviour
 {
-    private CharacterController characterController;
+    [SerializeField] private CharacterController characterController;
+    [SerializeField] private CinemachineVirtualCamera virtualCamera = null;
     private Vector3 move;
     private float speed=5;
     private float  verticalSpeed;
     private bool isGround;
-    private Rigidbody rigidbody;
     void Start()
     {
         characterController = GetComponent<CharacterController>();
     }
 
- 
-    void Update()
+    public override void OnStartAuthority()
     {
+        enabled = true;
+
+    }
+    [ClientCallback]
+    private void Update() {
         if (isLocalPlayer)
         {
-        PlayMove();
+         PlayMove();
 
         }
-      
     }
+    [Client]
     private void PlayMove() {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
         move = new Vector3(h,0,v)*Time.deltaTime*speed;
+       // move = virtualCamera.transform.TransformDirection(move);
         if (move.x!=0||move.z!=0)
         {
             move.y = 0;
             transform.rotation = Quaternion.RotateTowards(transform.rotation,Quaternion.LookRotation(move),100);
 
         }
-        characterController.Move(move);
-
-    
+        characterController.Move(move);    
     }
     private void CaulateVerticalSpeed() {
         if (characterController.isGrounded)
